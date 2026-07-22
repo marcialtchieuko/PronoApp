e1fa6b4ecbed704f4dc007bfddd37ce831d812d3
marcialtchieuko <tchieukomarcial@gmail.com>
2026-07-22 06:22:06 +0100

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PronoApp from "./PronoApp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PronoApp />
-  </React.StrictMode>
-);
